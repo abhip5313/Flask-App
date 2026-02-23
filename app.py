fd0f96139665c5a355d22c1ff6359c72b07231f5
Abhishek Pawar <abhi9158014229@gmail.com>
2026-02-23 12:31:30 +0530

    update code

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def register():
     return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = {
+        "fullname": request.form.get("fullname"),
+        "email": request.form.get("email"),
+        "phone": request.form.get("phone"),
+        "username": request.form.get("username"),
+        "password": request.form.get("password"),
+    }
+    return render_template("success.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
